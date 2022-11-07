ebe444db938964ae87d854ac6e71fc757ade966f
Imran Akhtar <imran.akhtar@brainxtech.com>
2022-11-07 18:24:21 +0500

    RMS-15 testing

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,9 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "imran.akhtar.pmdc@gmail.com"
   layout "mailer"
-  def welcome_email
-    @user = params[:user]
-    @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-  end
 end
