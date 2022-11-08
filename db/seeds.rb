3b7655cde1e23950675f8b208037c4e8d1f30fb6
Imran Akhtar <imran.akhtar@brainxtech.com>
2022-11-08 15:35:14 +0500

    make it better

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,21 @@
 # Examples:
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Form.create(_type: :"medical", data: {consultant: "Dr.imran", patient: "umar", description: "Throat infection", fees: "2000", total: "2000"})
-User.create(email: :"imran.akhtar@gmail.com", password: :"qwerty", emp_id: :"0137", name: :"Imran Akhtae", phone: 03340053, department: :"Ror")
 
-User.create(email: :"umair@gmail.com", password: :"qwerty", emp_id: :"0111", name: :"Umair", phone: 03340053, department: :"Ror")
+User.create(email: :"imran.akhtar@mailinator.com", password: :"qwerty", emp_id: :"0137", name: :"Imran Akhtae", phone: 03340053, department: :"Ror")
+User.create(email: :"soban.akbar@mailinator.com", password: :"qwerty", emp_id: :"0222", name: :"Soban Akbar", phone: 03340053, department: :"Accounts")
+User.create(email: :"tahir@mailinator.com", password: :"qwerty", emp_id: :"01131", name: :"Tahir", phone: 03340053, department: :"Ror")
+User.create(email: :"soban.akram@mailinator.com", password: :"qwerty", emp_id: :"01131", name: :"Soban Akrm", phone: 03340053, department: :"Ror")
 
 Role.create(_type: :software_engineer , medical_allowance: 1500 , travel_allowance: 1000 , fitness_allowance: 32330, dinner_allowance: 3000)
-
 Role.create(_type: :project_manager , medical_allowance: 15000 , travel_allowance: 1000 , fitness_allowance: 32330, dinner_allowance: 3000)
-
-User.create(email: :"soban.akbar@gmail.com", password: :"qwerty", emp_id: :"0222", name: :"Soban Akbar", phone: 03340053, department: :"Accounts")
-
 Role.create(_type: :admin , medical_allowance: 15000 , travel_allowance: 1000 , fitness_allowance: 32330, dinner_allowance: 3000)
+Role.create(_type: :accountant , medical_allowance: 15000 , travel_allowance: 1000 , fitness_allowance: 32330, dinner_allowance: 3000)
+
+
+Form.create(_type: :"medical", data: {consultant: "Dr.imran", patient: "umar", description: "Throat infection", fees: "2000", total: "2000"})
 Form.create(_type: :"working", data: {"reporting_manager":"", date:"" ,day:"", reason_of_working:"", nature_of_holiday:"",amount:""})
-Form.create(_type: :"expense", data: {project_name: "..", details: "..", amount: "..."})
+Form.create(_type: :"expense", data: {project_name: "..", details: "", amount: ""})
 Form.create(_type: :"general", data: {description: "", amount: ""})
 
 Submission.create()
